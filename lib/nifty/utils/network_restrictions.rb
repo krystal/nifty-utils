e2898728a7d990842e60f5bbc7a9399fba85f5db
Adam Cooke <adam@atechmedia.com>
2015-03-08 18:23:00 +0000

    add network restrictions

---
+++ lib/nifty/utils/network_restrictions.rb
@@ -0,0 +1,28 @@
+require 'ipaddr'
+
+module Nifty
+  module Utils
+    module NetworkRestrictions
+
+      def self.ip_in_networks?(ip, networks = [])
+        !!network_for_ip(networks, ip)
+      end
+
+      def self.network_for_ip(networks, ip)
+        networks.each do |i|
+          if IPAddr.new(i).include?(ip)
+            return i
+          end
+        end
+        return nil
+      rescue ArgumentError => e
+        if e.message == 'invalid address'
+          return nil
+        else
+          raise
+        end
+      end
+
+    end
+  end
+end
